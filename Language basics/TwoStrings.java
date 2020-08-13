7e19fb5c0f38c07e838d8aa96967e647868046fb
sankrish <66157644+santhinikrish@users.noreply.github.com>
2020-08-13 23:25:49 +0530

    Add files via upload

---
+++ Language basics/TwoStrings.java
@@ -0,0 +1,16 @@
+package LanguageBasics;
+
+import java.util.Scanner;
+
+public class TwoStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String string1=sc.next();
+		String string2=sc.next();
+		System.out.println(string1+" Technologies "+string2);
+		sc.close();
+	}
+
+}
